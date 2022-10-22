3788af554edabc0a42a1c87d02d386dd293681b0
Veysel <veyselkartalms@gmail.com>
2022-10-22 21:51:32 +0300

    edit theme

--- src/components/LandingBanner/LandingBanner.jsx
+++ src/components/LandingBanner/LandingBanner.jsx
@@ -1,27 +1,31 @@
 import React from 'react'
 import "./landingBanner.scss";
+import Header from '../Header/Header';
 import { clock, wallet, rocket } from '../../assets/img';
 
 const LandingBanner = () => {
     return (
-        <div className="landing-banner">
-            <h4>Fast. Responsive. Free.</h4>
-            <div className="decors">
-                <div className="clock">
-                    <img src={clock} alt="" />
+        <>
+            <Header />
+            <div className="landing-banner">
+                <h4>Fast. Responsive. Free.</h4>
+                <div className="decors">
+                    <div className="clock">
+                        <img src={clock} alt="clock" />
+                    </div>
+                    <div className="wallet">
+                        <img src={wallet} alt="wallet" />
+                    </div>
+                    <div className="rocket">
+                        <img src={rocket} alt="rocket" />
+                    </div>
                 </div>
-                <div className="wallet">
-                    <img src={wallet} alt="" />
-                </div>
-                <div className="rocket">
-                    <img src={rocket} alt="Clock" />
+                <div className="scroll-down">
+                    take a look.
+                    <svg width="20.083" height="10.926" viewBox="0 0 20.083 10.926"><path d="M15.84,0,9.32,6.52a1.986,1.986,0,0,1-2.8,0L0,0" transform="translate(2.121 2.121)" /></svg>
                 </div>
             </div>
-            <div className="scroll-down">
-                take a look.
-                <svg width="20.083" height="10.926" viewBox="0 0 20.083 10.926"><path d="M15.84,0,9.32,6.52a1.986,1.986,0,0,1-2.8,0L0,0" transform="translate(2.121 2.121)" /></svg>
-            </div>
-        </div>
+        </>
     )
 }
 
