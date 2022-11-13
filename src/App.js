9f20da9b08d44c8fe0252914d2dca1efd2d05a99
Veysel <veyselkartalms@gmail.com>
2022-11-13 17:47:06 +0300

    theme1 removed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import { Link, Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import LandingPage from "./pages/LandingPage/LandingPage";
-import Theme1 from "./themes/Theme-1/Theme1";
 
 function App() {
   return (
@@ -8,7 +7,6 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/theme1" element={<Theme1 />} />
         </Routes>
       </BrowserRouter>
     </>
--- src/themes/Theme-1/assets/index.js
+++
@@ -1,3 +0,0 @@
-export { default as dots } from './dots.png';
-export { default as logo } from './logo.png';
-export { default as mail } from './mail.png';
